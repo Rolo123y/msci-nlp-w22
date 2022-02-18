c0d481b78fad7422869198c814d9cc8b56a20eea
Rolo123y <ruhollah123@hotmail.com>
2022-02-18 06:24:03 -0500

    use gensims word2vec to train on amazon corpus

--- a3/inference.py
+++ a3/inference.py
@@ -1,17 +1,37 @@
 import sys
 import time
+from gensim.models import KeyedVectors
 
 # generates the top-20 most similar words for a given word.
 
-def run(arg):
+def read_file(Path_To_txt):
+
+    with open(Path_To_txt, 'r') as f:
+        content = f.readlines()
+    all_content_rev = [word.strip() for word in content]
+
+    return all_content_rev
+
+def run(Path_To_txt):
+
+    all_words = read_file(Path_To_txt)
+
+    wv = KeyedVectors.load("a3//data//word2vec.wordvectors", mmap='r')
+
+    for word in all_words:
+        try:
+            print(f'{word} =>\t{wv.most_similar(positive=word, topn=20)}\n\n')
+        except:
+            print(word, '=> no similar words found.\n\n')
+
     return 1
 
 if __name__=="__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         print(f"only {len(sys.argv)} arguments provided. Please enter 'py .\a3\inference.py C:\\Path\\to\\afile.txt\nSESSION TERMINATED.")
         sys.exit(0)
     else:
         start_time = time.time()
-        run(sys.argv[1], sys.argv[2])
+        run(sys.argv[1])
         print("\nCOMPLETED IN --- %s seconds ---" % (time.time() - start_time))
-# py .\a2\inference.py C:\\Users\\Ruhol\\Desktop\\4BWinter2022\\MSCI598\\msci-nlp-w22\\a2\\data\\someSentences.txt mnb_uni
+# py .\a3\inference.py C:\\Users\\Ruhol\\Desktop\\4BWinter2022\\MSCI598\\msci-nlp-w22\\a3\\data\\testInference.txt
--- a3/main.py
+++ a3/main.py
@@ -1,10 +1,45 @@
 import sys
 import time
+import gensim
 
 # Write a python script using genism library to train a Word2Vec model on the Amazon corpus.
 # Output: Word2vec model trained on the entire Amazon corpus (pos.txt + neg.txt)
 
-def main(arg):
+def read_files(Path_To_files):
+
+    with open(Path_To_files+"\\pos.txt", "r") as f:
+        pos_content = f.readlines()
+
+    with open(Path_To_files+"\\neg.txt", "r") as f:
+        neg_content = f.readlines()
+    all_content = (pos_content + neg_content)
+    all_content_rev = [gensim.utils.simple_preprocess(sentence) for sentence in all_content]
+
+    return all_content_rev
+
+def main(Path_To_data):
+
+    # Preprocessing sentences
+    all_sentences = read_files(Path_To_data)
+
+    # Initialize model
+    Word2vec_model = gensim.models.Word2Vec(
+        window=10,
+        min_count=2,
+        workers=4,
+    )
+
+    Word2vec_model.build_vocab(all_sentences, progress_per=500)
+    print(f'model epochs: {Word2vec_model.epochs}')
+
+    # Training and saving word2vec model
+    Word2vec_model.train(all_sentences, total_examples=Word2vec_model.corpus_count, epochs=Word2vec_model.epochs)
+    Word2vec_model.save("a3//data//w2v.model")
+
+    # Savings wordvectors
+    word_vectors = Word2vec_model.wv
+    word_vectors.save("a3//data//word2vec.wordvectors")
+
     return 1
 
 if __name__=="__main__":
